2ec3c787449a2d006a9a35baf62ceb027cd701cc
HarmmerRay <zhao131804@gmail.com>
2025-05-13 16:46:15 +0800

    #[2025.5.13] 小红书抓取图文页面数据API接口畅通

--- app/api/endpoints/xhs_web.py
+++ app/api/endpoints/xhs_web.py
@@ -31,7 +31,7 @@ async def fetch_one_note(request: Request,
                           url: str = Query(example="https://www.xiaohongshu.com/explore/680b87f9000000000f03a497?xsec_token=ABIX--M-eqnHdnkohztWwEyIfFtE4YT3CUdTjCD82dK60=&xsec_source=pc_feed", description="作品url地址/Work URL")):
     try:
         print(url)
-        data = await XhsWebCrawler.fetch_one_video(url)
+        data = await XhsWebCrawler.fetch_one_note(url)
         return ResponseModel(code=200,
                              router=request.url.path,
                              data=data)
