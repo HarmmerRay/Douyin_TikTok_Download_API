d1c2306dfb9bc5ee301e055410dfa0ebc059a7dd
HarmmerRay <zhao131804@gmail.com>
2025-05-13 17:37:46 +0800

    #[2025.5.13] 小红书作品一级评论数据 API畅通

--- app/api/endpoints/xhs_web.py
+++ app/api/endpoints/xhs_web.py
@@ -42,3 +42,21 @@ async def fetch_one_note(request: Request,
                                     params=dict(request.query_params),
                                     )
         raise HTTPException(status_code=status_code, detail=detail.dict())
+
+# 获取作品的一级评论数据
+@router.get("/fetch_first_level_comments", response_model=ResponseModel, summary="获取作品一级评论数据/Get first level comments")
+async def fetch_first_level_comments(request: Request,
+                                     note_id: str = Query(example="680b87f9000000000f03a497", description="作品ID/Work ID"),
+                                     xsec_toekn: str =Query(example="ABGKRE1TQqLF8Txl4rcSOTWfVmTOimU7qra5R4sZrvOVc=", description="任意一个xsec_token即可，打开小红书网站在网址框内查看/Xsec_token") ):
+    try:
+        data = await XhsWebCrawler.fetch_first_level_comment(note_id,xsec_toekn)
+        return ResponseModel(code=200,
+                             router=request.url.path,
+                             data=data)
+    except Exception as e:
+        status_code = 400
+        detail = ErrorResponseModel(code=status_code,
+                                    router=request.url.path,
+                                    params=dict(request.query_params),
+                                    )
+        raise HTTPException(status_code=status_code, detail=detail.dict())
